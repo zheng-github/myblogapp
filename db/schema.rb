62119085c46bb228eceaf9c30c55a7d541ec935b
Zheng <zheng.wang@salesforce.com>
2017-09-12 11:43:56 -0700

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20170104005043) do
 
   # These are extensions that must be enabled in order to support this database
+  #
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
